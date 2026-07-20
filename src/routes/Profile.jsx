a25128b4077d77cd0120107b2162d6a20df89fc7
jema <smh1141@naver.com>
2026-07-20 14:43:13 +0900

    feat: 프로필 페이지에 로그아웃 버튼 추가 및 내비게이션 컴포넌트 생성

--- src/routes/Profile.jsx
+++ src/routes/Profile.jsx
@@ -1,7 +1,26 @@
+import { signOut } from "firebase/auth";
+import { authService } from "../firebase";
+import { Typography, Button } from "@mui/material";
+import { useNavigate } from "react-router";
 function Profile() {
+  const auth = authService;
+  const navigate = useNavigate();
+  const onLogout = () => {
+    signOut(auth)
+      .then(() => {
+        // Sign-out successful.
+        navigate("/");
+      })
+      .catch(error => {
+        // An error happened.
+      });
+  };
   return (
     <>
-      <h1>Profile</h1>
+      <h2>Profile</h2>
+      <Button variant="contained" type="button" sx={{ mt: 2 }} onClick={onLogout}>
+        로그아웃
+      </Button>
     </>
   );
 }
