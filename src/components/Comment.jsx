ebb9264df4e7a90e24847908ad1ba23ba637f116
jema <smh1141@naver.com>
2026-07-22 10:30:27 +0900

    feat: 댓글 삭제 시 이미지도 함께 삭제하도록 기능 추가

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,15 +1,25 @@
 import { ListItem, ListItemText, Button, Stack, Box, TextField } from "@mui/material";
-import { db } from "../firebase";
+import { db, storageService } from "../firebase";
 import { doc, deleteDoc, updateDoc } from "firebase/firestore";
 import { useState } from "react";
+import { ref, deleteObject } from "firebase/storage";
 
 export default function Comment({ item, isShown }) {
   const [edit, setEdit] = useState(false);
   const [comment, setComment] = useState(item.comment);
-
   const handleDelete = async () => {
     if (!window.confirm("정말 삭제할까요?")) return;
-    await deleteDoc(doc(db, "comments", item.id));
+    try {
+      await deleteDoc(doc(db, "comments", item.id));
+      if (item.image) {
+        const storage = storageService;
+        const desertRef = ref(storage, item.image);
+        await deleteObject(desertRef);
+      }
+    } catch (e) {
+      console.log("삭제 오류", e);
+      alert("삭제중 오류가 발생했습니다.");
+    }
   };
   const toggleEditMode = () => {
     setEdit(prev => !prev);
@@ -57,6 +67,22 @@ export default function Comment({ item, isShown }) {
             primary={item.comment} // 제목
             secondary={item.date?.toDate() ? item.date.toDate().toLocaleString() : "작성시간 없음"} // 출력할 내용
           />
+          {item.image && (
+            <Box sx={{ mr: 1, display: "flex", alignItems: "center", gap: 1 }}>
+              <Box
+                component="img"
+                src={item.image}
+                alt="미리보기"
+                sx={{
+                  width: 50,
+                  height: 50,
+                  objectFit: "cover",
+                  border: "1px solid #ddd",
+                  borderRadius: 3,
+                }}
+              ></Box>
+            </Box>
+          )}
           {isShown && (
             <Stack direction="row" spacing={1}>
               <Button variant="outlined" size="small" onClick={toggleEditMode}>
