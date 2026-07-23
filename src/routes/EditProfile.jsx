5c48171bc130ff711dca2273fdd0f4a33efa4635
jema <smh1141@naver.com>
2026-07-23 11:20:58 +0900

    feat : 프로필 페이지 생성중. 기본 프로필만 우선 정적으로 배치

--- src/routes/EditProfile.jsx
+++ src/routes/EditProfile.jsx
@@ -1,8 +1,23 @@
-function EditProfile() {
+import { Box, Button } from "@mui/material";
+import UploadFileIcon from "@mui/icons-material/UploadFile";
+
+function EditProfile({ setProfileEditMode }) {
   return (
-    <>
-      <h1>EditProfile</h1>
-    </>
+    <Box sx={{ mt: 1, display: "flex", alignItems: "center", gap: 1 }}>
+      <Button component="label" variant="outlined" type="button" startIcon={<UploadFileIcon />}>
+        이미지 선택
+        <input type="file" hidden accept="image/*" />
+      </Button>
+      <Button
+        variant="contained"
+        type="button"
+        onClick={() => {
+          setProfileEditMode(false);
+        }}
+      >
+        취소
+      </Button>
+    </Box>
   );
 }
 
--- src/routes/Profile.jsx
+++ src/routes/Profile.jsx
@@ -1,12 +1,15 @@
 import { signOut } from "firebase/auth";
 import { db, authService } from "../firebase";
-import { Typography, Button, Divider, List } from "@mui/material";
+import { Typography, Button, Divider, List, Box } from "@mui/material";
 import { useNavigate } from "react-router";
 import { useEffect, useState } from "react";
 import { collection, query, orderBy, getDocs, onSnapshot, where } from "firebase/firestore";
 import Comment from "../components/Comment";
+import EditProfile from "./EditProfile";
+
 function Profile() {
   const [comments, setComments] = useState([]);
+  const [profileEditMode, setProfileEditMode] = useState(false);
   const auth = authService;
   const userId = auth.currentUser.uid;
   const navigate = useNavigate();
@@ -44,6 +47,26 @@ function Profile() {
   return (
     <>
       <h2>Profile</h2>
+      <div className="profile">
+        <img src="/profile_icon.png" alt="기본 프로필" />
+      </div>
+      {profileEditMode ? (
+        <EditProfile setProfileEditMode={setProfileEditMode} />
+      ) : (
+        <Box sx={{ mt: 1, display: "flex", alignItems: "center", gap: 1 }}>
+          <Button
+            variant="outlined"
+            type="button"
+            sx={{ mt: 2 }}
+            onClick={() => {
+              setProfileEditMode(true);
+            }}
+          >
+            프로필 이미지 변경
+          </Button>
+        </Box>
+      )}
+      <Divider sx={{ mt: 2 }} />
       <Button variant="contained" type="button" sx={{ mt: 2 }} onClick={onLogout}>
         로그아웃
       </Button>
